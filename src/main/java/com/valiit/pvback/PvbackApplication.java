35898fe614fd4b3761f3ca77e63c59fe6c22a172
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-10 11:23:48 +0300

    Initial commit

---
+++ src/main/java/com/valiit/pvback/PvbackApplication.java
@@ -0,0 +1,13 @@
+package com.valiit.pvback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valiit/pvback/PvbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valiit.pvback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
