380b38d6c80922700312d5f30571579812e2851a
peeter august <peeterlehtz@gmail.com>
2024-04-14 21:04:39 +0300

    Login Done

--- src/main/java/com/valiit/pvback/business/login/LoginController.java
+++ src/main/java/com/valiit/pvback/business/login/LoginController.java
@@ -1,7 +1,6 @@
 package com.valiit.pvback.business.login;
 
 import com.valiit.pvback.business.login.dto.LoginResponse;
-import com.valiit.pvback.domain.user.UserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class LoginController {
 
-    private final UserMapper userMapper;
     private LoginService loginService;
 
     @GetMapping("/login")
     public LoginResponse login(@RequestParam String email, @RequestParam String password) {
         return loginService.login(email, password);
     }
-
-
-
 
     // todo: raja kaardistus HTTP  meetod (GET, POST, jne) ja URL
     // todo: meetodi nimi
@@ -29,5 +24,4 @@ public class LoginController {
     // todo: l6puks tekib mingi objekt mingist klassist (loginResponse, mida see meetod tagastab) aga alguses progedes void meetod
     // todo: actionit siin ei tee
     // todo: samanimelised meetodid
-
 }
--- src/main/java/com/valiit/pvback/business/login/dto/LoginResponse.java
+++ src/main/java/com/valiit/pvback/business/login/dto/LoginResponse.java
@@ -4,7 +4,6 @@ import com.valiit.pvback.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
 
 import java.io.Serializable;
 
