7c892e9674187d7623b93ddc133244bc869ec4ee
joonas <joonas.kaal@gmail.com>
2024-04-15 10:42:58 +0300

    Loginservice, UerMapper, Validation service WIP

---
+++ src/main/java/com/valiit/pvback/domain/user/UserMapper.java
@@ -0,0 +1,15 @@
+package com.valiit.pvback.domain.user;
+
+import com.valiit.pvback.business.login.dto.LoginResponse;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+
+    @Mapping(source = "id", target = "userId")
+    @Mapping(source = "role", target = "roleName")
+    LoginResponse toLoginResponse(User user);
+
+
+}
