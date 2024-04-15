e839307bc496c3a77b132a24fdad3d3b2583600e
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-15 10:42:29 +0300

    Login WIP

--- src/main/java/com/valiit/pvback/domain/user/UserMapper.java
+++ src/main/java/com/valiit/pvback/domain/user/UserMapper.java
@@ -6,6 +6,6 @@ import org.mapstruct.*;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
     @Mapping(source = "id", target = "userId")
-    @Mapping(source = "role.role", target = "roleName")
+    @Mapping(source = "role", target = "roleName")
     LoginResponse toLoginResponse(User user);
 }
