acc3709059eb2d5202b665502f8cb641707b92d4
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-25 09:19:24 +0300

    Add companyUser done

--- src/main/java/com/valiit/pvback/business/Status.java
+++ src/main/java/com/valiit/pvback/business/Status.java
@@ -3,4 +3,5 @@ package com.valiit.pvback.business;
 public class Status {
     public static final String ACTIVE = "A";
     public static final String DEACTIVATED = "D";
+    public static final String SUPERUSER = "S";
 }
