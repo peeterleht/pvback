8122040ab3cbf945fb9a73853c2bb3e1083a7e68
Se <s@a.ee>
2024-04-26 15:02:59 +0300

    WIP

--- src/main/java/com/valiit/pvback/business/projectoverview/ProjectOverviewController.java
+++ src/main/java/com/valiit/pvback/business/projectoverview/ProjectOverviewController.java
@@ -1,8 +1,10 @@
 package com.valiit.pvback.business.projectoverview;
 
 import com.valiit.pvback.business.projectoverview.dto.ProjectOverviewInfos;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,8 @@ public class ProjectOverviewController {
 
     private final ProjectOverviewService projectOverviewService;
 
-    @GetMapping("/project/overview")
-    public ProjectOverviewInfos getProjectOverview(@RequestParam Integer projectId) {
+    @GetMapping("/project/{projectId}")
+    public ProjectOverviewInfos getProjectOverview(@PathVariable @Valid Integer projectId) {
         return projectOverviewService.getProjectOverview(projectId);
     }
 }
